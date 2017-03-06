5f1db66963220c883ed96f10498621a8b345b3c8
david <cyber_veio@hotmail.com>
2017-03-06 17:36:36 +0000

    Print and plot from epic

--- ticker.py
+++ ticker.py
@@ -1,18 +1,21 @@
 import os
-from time import sleep
+from datetime import datetime
 import requests
 from urllib.parse import urljoin
 import json
 
-import credentials
+import matplotlib.pyplot as plt
+
+import credentials as cred
 
 class Client(object):
-    def __init__(self, username, password):
+    def __init__(self, username, password, api_key):
         self.base_url = "https://demo-api.ig.com/"
         self.watchlists_url = urljoin(self.base_url, "gateway/deal/watchlists/")
-        self.api_key = "1f4a7f4278c35d01ce561326abdd2d7433fee8f4"
-        self.username = "VEIO_DEMO"
-        self.password = "UzZ4mObXT929"
+        self.instrument_url = urljoin(self.base_url, "gateway/deal/markets/")
+        self.api_key = api_key
+        self.username = username
+        self.password = password
         self.headers = {"X-IG-API-KEY": self.api_key}
 
     def connect(self):
@@ -37,19 +40,53 @@ class Client(object):
         r = requests.get(url, headers=self.headers)
         return json.loads(r.content.decode())["markets"]
 
-    def display_selected_instruments(self, watchlist="Popular%20Markets"):
+    def display_watchlist_instruments(self, watchlist="Popular%20Markets"):
         """Prints out the instruments information from a given watchlist"""
         for instrument in self.get_instruments(watchlist):
-            print("{:25.25} : {:6.6} {:6.6} {}".format(
+            print("{:25.25} : {:6.6}/{:6.6} {}".format(
                                            instrument["instrumentName"],
                                            str(instrument["offer"]),
                                            str(instrument["bid"]),
                                            str(instrument["updateTimeUTC"])))
 
+    def get_from_epic(self, epic):
+        url = urljoin(self.instrument_url, epic)
+        r = requests.get(url, headers=self.headers)
+        data = json.loads(r.content.decode())
+        rtn = dict()
+        rtn['name'] = data['instrument']['name']
+        rtn['bid'] = data['snapshot']['bid']
+        rtn['offer'] = data['snapshot']['offer']
+        rtn['high'] = data['snapshot']['high']
+        rtn['low'] = data['snapshot']['low']
+        rtn['percent'] = data['snapshot']['percentageChange']
+        rtn['time'] = data['snapshot']['updateTime']
+        return rtn
+
+    def display_from_epic(self, epic):
+        data = self.get_from_epic(epic)
+        print("{:20.20}: {:6.6}/{:6.6} High:{:6.6} Low:{:6.6} %{} {}".format(
+            data['name'], data['offer'], data['bid'],
+            data['high'], data['low'], data['percent'], data['time']))
+        return data
 if __name__ == "__main__":
-    client = Client(credentials.username, credentials.password)
+    client = Client(cred.username, cred.password, cred.api_key)
     client.connect()
+    offers = []
+    bids = []
+    t = []
+    plt.ion()
+    fig, ax = plt.subplots()
     while True:
         os.system('clear')
-        client.display_selected_instruments()
-        sleep(10)
+        # client.display_watchlist_instruments()
+        data = client.display_from_epic("IX.D.FTSE.CFD.IP")
+        offers.append(data['offer'])
+        bids.append(data['bid'])
+        t.append(datetime.now())
+        ax.plot(t, offers)
+        ax.plot(t, bids)
+        plt.gcf().autofmt_xdate()
+        plt.show()
+        plt.pause(10)
+        ax.clear()
